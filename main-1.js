c79ea188cb4e9ecff4076eb2bcdc02125aeeb7af
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-05-15 17:19:26 +0500

    up

--- main-1.js
+++ main-1.js
@@ -1,46 +1,44 @@
-// // 1
-// function minutesToHours(minutes) {
-//     return minutes / 60
-// }
+// 1
+function minutesToHours(minutes) {
+    return minutes / 60
+}
 
-// export { minutesToHours };
+export { minutesToHours };
 
-// // 2
-// function averageOf4Numbers(nr1, nr2, nr3, nr4) {
-//     return (nr1 + nr2 + nr3 + nr4) / 4
-// }
+// 2
+function averageOf4Numbers(nr1, nr2, nr3, nr4) {
+    return (nr1 + nr2 + nr3 + nr4) / 4
+}
 
-// export { averageOf4Numbers };
+export { averageOf4Numbers };
 
-// // 3
-// function concat3(string1, string2, string3, separator) {
-//     let arr = []
-//     arr.push(string1);
-//     arr.push(string2);
-//     arr.push(string3);
-//     let arr1 = arr.join(separator)
-//     return arr1
-// }
+// 3
+function concat3(string1, string2, string3, separator) {
+    let arr = []
+    arr.push(string1);
+    arr.push(string2);
+    arr.push(string3);
+    let arr1 = arr.join(separator)
+    return arr1
+}
 
-// export { concat3 };
+// -------------------------------------------------------------------------
+// 4
+function getMonthsNeededToWait(index1, index2) {
+    if (index1 < index2) {
+        return index2 - index1
+    } else {
+        return 12 - (index1 - index2)
+    }
+}
+console.log(getMonthsNeededToWait(11, 0));
 
-// // 4
-// function getMonthsNeededToWait(index1, index2) {
-//     if (index1 < index2) {
-//         return index2 - index1
-//     } else {
-//         return 12 - (index1 - index2)
-//     }
-// }
-
-// export { getMonthsNeededToWait };
-
-// // 6
-// function getGasolineAmount(distance, consumptionPer100Km) {
-//     return (distance / 100) * consumptionPer100Km
-// }
-
-// export { getGasolineAmount };
+// -------------------------------------------------------------------------
+// 6
+function getGasolineAmount(distance, consumptionPer100Km) {
+    return (distance / 100) * consumptionPer100Km
+}
+// console.log(getGasolineAmount(160,6.4));
 
 function lastNRevert(text, n) {
     let arr = [];
@@ -50,5 +48,49 @@ function lastNRevert(text, n) {
     }
     arr=arr.join("")
     return arr
-} console.log(lastNRevert("Jon Doe",3));
+}
+// console.log(lastNRevert("Salom Dunyo", 3));
+
+// -------------------------------------------------------------------------
+// 7
+function getBusinessAddress(business) {
+    return `${business.address.street}, number ${business.address.number}, ${business.address.zipCode}`
+}
+
+let obj = {
+    address: { number: 3, street: "Avenuepark", zipCode: 123500 },
+    name: "100 Functions ltd."
+}
+// console.log(getBusinessAddress(obj));
+
+// -------------------------------------------------------------------------
+// 8
+function getUserObject(firstName, lastName, age) {
+    return {
+        age: age,
+        name: `${firstName} ${lastName}`
+    }
+}
+console.log(getUserObject("Komol", "Parpixodjayev", 27));
+// -------------------------------------------------------------------------
+
+// 9
+function canDriveCar(user, car) {
+    if (user.age >= 18 || car.engineSize < 1000) {
+        return true
+    }
+    return false
+}
+// -------------------------------------------------------------------------
+
+// 10
+function areAllNumbersEven(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 != 0) {
+            return false
+        }
+    }
+    return true
+}
+
 
